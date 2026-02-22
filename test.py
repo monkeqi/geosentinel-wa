5ab0809cdae1deb9e4520d67768bc2df6969df70
unknown <josephbell100@protonmail.com>
2026-02-21 19:21:35 -0800

    Initial commit - environment test

---
+++ test.py
@@ -0,0 +1,9 @@
+import geopandas as gpd
+import rasterio
+import sklearn
+import anthropic
+import streamlit
+import folium
+
+print("GeoSentinel WA - Environment Ready!")
+print(f"GeoPandas version: {gpd.__version__}")
